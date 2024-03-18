427629dbca9ce80e85ad4893a0802648cb440857
Amirreza Jahantab <jahantab83@gmail.com>
2024-03-18 17:10:38 +0330

    Create View

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+
+    address = models.CharField(max_length=1000,null=True,blank=True)
+    name = models.CharField(max_length=1000,null=True,blank=True)
+    
+    from django.db import models
+
+    def __str__(self):
+        return self.name if self.name else self.address
